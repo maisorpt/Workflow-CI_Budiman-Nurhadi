d3f4a911c27afae3dcb77ee69116f3db9aa5c2c7
Maisorpt <maisorpt@gmail.com>
2026-06-09 23:32:31 +0700

    deleting mlflow.run

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -9,11 +9,10 @@ if __name__ == "__main__":
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 5
     dataset_path = sys.argv[3] if len(sys.argv) > 3 else "btc_preprocessing.csv"
 
-    mlflow.set_experiment("Eksperimen Klasifikasi Arah Tren BTC")
 
     print(f"Membaca dataset: {dataset_path}...")
     
-    # 2. LOAD DATASET BERDASARKAN PARAMETER
+    # 2. Load Dataset
     data = pd.read_csv(dataset_path)
 
     if 'Unnamed: 0' in data.columns:
@@ -26,34 +25,33 @@ if __name__ == "__main__":
     X = data.drop(columns=kolom_di_drop)
     y = data["Target"].astype(int)
 
-    # 3. TRAIN-TEST SPLIT
+    # 3. Train-Test Split (Kronologis)
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, shuffle=False
     )
 
     input_example = X_train.iloc[0:5]
 
-    # 4. TRAINING & MLFLOW AUTOLOG
-    with mlflow.start_run(run_name="MLProject_CI_Run"):
-        mlflow.sklearn.autolog()
-        
-        # MENGGUNAKAN PARAMETER DARI MLProject
-        model = RandomForestClassifier(
-            n_estimators=n_estimators, 
-            max_depth=max_depth,
-            min_samples_leaf=5,
-            random_state=42
-        )
-        
-        model.fit(X_train, y_train)
-        
-        mlflow.sklearn.log_model(
-            sk_model=model,
-            artifact_path="model",
-            input_example=input_example
-        )
+    mlflow.sklearn.autolog()
+    
+    model = RandomForestClassifier(
+        n_estimators=n_estimators, 
+        max_depth=max_depth,
+        min_samples_leaf=5,
+        random_state=42
+    )
+    
+    print("Memulai proses training model...")
+    model.fit(X_train, y_train)
+    
+    # Log Model & Artefak
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        input_example=input_example
+    )
 
-        test_accuracy = model.score(X_test, y_test)
-        mlflow.log_metric("test_accuracy", test_accuracy)
+    test_accuracy = model.score(X_test, y_test)
+    mlflow.log_metric("test_accuracy", test_accuracy)
 
-    print(f"Proses Run MLProject Selesai! Akurasi: {test_accuracy:.4f}")
+    print(f"Proses Run MLProject Selesai! Akurasi Akhir: {test_accuracy:.4f}")
